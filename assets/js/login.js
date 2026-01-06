6af85792bb5ec840e0535a8052a184adb929b8be
naseuri <93072217+naseri3@users.noreply.github.com>
2026-01-06 09:57:11 +0900

    naver login push

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,21 +3,38 @@ const GOOGLE_CLIENT_ID = "24355175704-aviumsce0orbnutandgjjsruphqca8g5.apps.goog
 const REDIRECT_URI = "https://aptjob.netlify.app/login.html";
 
 const GOOGLE_AUTH_URL =
-  "https://accounts.google.com/o/oauth2/v2/auth" +
-  "?client_id=" + GOOGLE_CLIENT_ID +
-  "&redirect_uri=" + encodeURIComponent(REDIRECT_URI) +
-  "&response_type=code" +
-  "&scope=openid email profile";
+    "https://accounts.google.com/o/oauth2/v2/auth" +
+    "?client_id=" + GOOGLE_CLIENT_ID +
+    "&redirect_uri=" + encodeURIComponent(REDIRECT_URI) +
+    "&response_type=code" +
+    "&scope=openid email profile";
 
 const googleBtn = document.querySelector(".google_login");
 
 googleBtn.addEventListener("click", () => {
-  window.location.href = GOOGLE_AUTH_URL;
+    window.location.href = GOOGLE_AUTH_URL;
 });
 const params = new URLSearchParams(window.location.search);
 const authCode = params.get("code");
 
 if (authCode) {
-  console.log("구글 로그인 성공, code:", authCode);
-  alert("구글 로그인 성공!");
+    console.log("구글 로그인 성공, code:", authCode);
+    alert("구글 로그인 성공!");
 }
+
+
+/** 네이버 로그인 API */
+var naver_id_login = new naver_id_login("hLO6jennO8FmeKMz2ntZ", "https://aptjob.netlify.app/login.html");
+var state = naver_id_login.getUniqState();
+naver_id_login.setButton("white", 2, 40);
+naver_id_login.setDomain("https://aptjob.netlify.app");
+naver_id_login.setState(state);
+naver_id_login.setPopup();
+naver_id_login.init_naver_id_login();
+naver_id_login.getLoginStatus(function (status) {
+    if (status) {
+        console.log(naver_id_login.user);
+    } else {
+        console.log("네이버 로그인 실패");
+    }
+});
